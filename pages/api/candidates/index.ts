b898cdb01e8cfd00fc48c433b010aabd0eff8180
AguirreEze <aguirre.eze95@gmail.com>
2022-03-11 11:11:17 -0300

    Feat endpoint for base data

--- pages/api/candidates/index.ts
+++ pages/api/candidates/index.ts
@@ -1,9 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Candidate } from "types/candidate";
 
+const initialData: Candidate[] = [
+  {
+    id: "goncy",
+    name: "Gonzalo Pozzo",
+    step: "Entrevista técnica",
+    comments: "Medio pelo",
+  },
+  {
+    id: "doe",
+    name: "John Doe",
+    step: "Entrevista inicial",
+    comments: "",
+  },
+];
+
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Candidate>
+  res: NextApiResponse<Candidate[]>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json(initialData);
 }
