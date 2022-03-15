c1366da5303a85e26eedf73f83e19fce180d9ad4
AguirreEze <aguirre.eze95@gmail.com>
2022-03-15 11:46:16 -0300

    Feat button functionality

--- components/candidate/index.tsx
+++ components/candidate/index.tsx
@@ -1,17 +1,47 @@
-import { CandidateType } from "types/candidate";
+import { CandidateType, StepType } from "types/candidate";
 import styles from "./styles.module.scss";
 interface Iprops {
   candidate: CandidateType;
+  updateCandidate: Function;
 }
+const steps: typeof StepType[] = [
+  "Entrevista inicial",
+  "Entrevista técnica",
+  "Oferta",
+  "Asignación",
+  "Rechazo",
+];
 
-export default function Candidate({ candidate }: Iprops) {
+export default function Candidate({ candidate, updateCandidate }: Iprops) {
+  const getUpdatedCandidate = (newStep: typeof StepType): CandidateType => {
+    return { ...candidate, step: newStep };
+  };
+
+  const moveLeft = () => {
+    const currentStep: number = steps.findIndex(
+      (step) => step === candidate.step
+    );
+    const stepIndex: number = currentStep === 0 ? 0 : currentStep - 1;
+    updateCandidate(getUpdatedCandidate(steps[stepIndex]));
+  };
+  const moveRight = () => {
+    const currentStep: number = steps.findIndex(
+      (step) => step === candidate.step
+    );
+    const stepIndex: number = currentStep === 4 ? 4 : currentStep + 1;
+    updateCandidate(getUpdatedCandidate(steps[stepIndex]));
+  };
   return (
     <li className={styles.card}>
       <h3 className={styles.name}>{candidate.name}</h3>
       <p className={styles.comments}>{candidate.comments}</p>
       <div className={styles.buttons}>
-        <button>{"<"}</button>
-        <button>{">"}</button>
+        {candidate.step !== "Entrevista inicial" && (
+          <button onClick={moveLeft}>{"<"}</button>
+        )}
+        {candidate.step !== "Rechazo" && (
+          <button onClick={moveRight}>{">"}</button>
+        )}
       </div>
     </li>
   );
