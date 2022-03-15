1f64d870617b476df6c40baaa5f5dd61c854a5d2
AguirreEze <aguirre.eze95@gmail.com>
2022-03-15 13:27:08 -0300

    Fix close form on submit

--- components/CandidateForm/index.tsx
+++ components/CandidateForm/index.tsx
@@ -21,6 +21,7 @@ export default function CandidateForm({ addCandidate }: Iprops) {
       step: "Entrevista inicial",
     };
     addCandidate && addCandidate(dataToSave);
+    setShowForm(false);
     setName("");
     setComments("");
   };
